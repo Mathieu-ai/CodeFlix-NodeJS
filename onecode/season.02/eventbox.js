e9f2ac134213329c50988f98787d2e46b2ae8090
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-10-12 11:01:00 +0200

    Season.02-Ex01.Part1

---
+++ onecode/season.02/eventbox.js
@@ -0,0 +1,9 @@
+var events = require('events')
+
+function empty() {
+    var eventEmitter = new events.EventEmitter();
+    eventEmitter.addListener('hi', () => console.log("hi") );
+    eventEmitter.emit('hi');
+}
+
+empty()
